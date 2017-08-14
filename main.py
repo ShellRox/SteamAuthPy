83358d5915501287d6020a2fa386f47cf320c656
ShellRox <georgechakhnashvili@gmail.com>
2017-08-14 15:45:12 +0400

    simplified controller

--- main.py
+++ main.py
@@ -5,50 +5,54 @@ from authentication.guard import SteamGuard
 from authentication.login import Login
 import json
 
-class Request(object):
-    def requestGuide(self):
-        print(
-            """
-            All Functions:
-                
-                Show guide - requestGuide();\n                       
-                Get access token - requestOauth(*path);\n          
-                Generate device id - requestDeviceId(steamid);\n           
-                Add authenticator - requestSetup(steamid, access_token, device id);\n
-                List logged secret keys - requestSecrets();\n
-                Generate two-factor authentication code - requestCode(time, secret_key);\n
-                Send login request - requestLogin(username, password, secret_key);\n
-                
-            """
-        )
-    def requestOauth(*p):
-        if len(p) < 1:
-            finder(path="authentication/steamchat.html")
-        else:
-            finder(path=str(p))
+def guide(self):
+    print(
+        """
+        All Functions:
+            
+            Show guide - requestGuide();\n                       
+            Get access token - requestOauth(*path);\n          
+            Generate device id - requestDeviceId(steamid);\n           
+            Add authenticator - requestSetup(steamid, access_token, device id);\n
+            List logged secret keys - requestSecrets();\n
+            Generate two-factor authentication code - requestAuthCode(time, secret_key);\n
+            Generate trade offer confirmation key - requestConfKey(time, secret_key, tag_name);\n
+            Send login request - requestLogin(username, password, secret_key);\n
+            
+        """
+    )
+def oauth(*p):
+    if len(p) < 1:
+        finder(path="authentication/steamchat.html")
+    else:
+        finder(path=str(p))
 
-    def requestDeviceId(self, steamid):
-        if len(str(steamid)) == 17 and str(steamid).isdigit():
-            return getDeviceId(str(steamid))
-        else:
-            raise ValueError("Please enter proper steamId")
+def deviceid(steamid):
+    if len(str(steamid)) == 17 and str(steamid).isdigit():
+        return getDeviceId(str(steamid))
+    else:
+        raise ValueError("Please enter proper steamId")
 
-    def requestSetup(self, steamId, Oauth, UDID, logging=True):
-        if len(str(steamId)) == 17 and str(steamId).isdigit():
-            addAuth = AddAuthenticator()
-            return addAuth.registerAuthenticator(steamId, Oauth, UDID)
-        else:
-            raise ValueError("Please enter proper steamId")
+def setup(steamId, Oauth, UDID, logging=True):
+    if len(str(steamId)) == 17 and str(steamId).isdigit():
+        addAuth = AddAuthenticator()
+        return addAuth.registerAuthenticator(steamId, Oauth, UDID)
+    else:
+        raise ValueError("Please enter proper steamId")
 
-    def requestSecrets(self):
-        with open('authentication/shared_secrets.json', 'r') as data:
-            return json.load(data)
+def secrets(self):
+    with open('authentication/shared_secrets.json', 'r') as data:
+        return json.load(data)
 
-    def requestCode(self, tm, secret):
-        obj = SteamGuard()
-        return obj.get_confirmation_key(tm, str(secret))
+def authcode(secret):
+    obj = SteamGuard()
+    return obj.get_auth_code(int(tm), str(secret))
 
-    def requestLogin(self, username, password, shared_secret):
-        login = Login(str(username), str(password), str(shared_secret))
-        return login.getInfo()
+def confkey(secret, tag):
+    obj = SteamGuard()
+    return obj.get_confirmation_key(int(tm), str(secret), tag)
+
+def login(username, password, shared_secret):
+    _login = Login(str(username), str(password), str(shared_secret))
+    return _login.getInfo()
 
